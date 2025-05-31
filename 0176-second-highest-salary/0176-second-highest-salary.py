e3616785bf1a52bd0902b40dc74341ad97fdf19b
RBENJAMINFRANKLIN <30905462+RBENJAMINFRANKLIN@users.noreply.github.com>
2025-05-31 15:12:43 +0530

    Time: 231 ms (97.81%), Space: 67.4 MB (36.46%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.py
+++ 0176-second-highest-salary/0176-second-highest-salary.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 def second_highest_salary(employee: pd.DataFrame) -> pd.DataFrame:
     n=2
-    emp1 =  employee['salary'].sort_values(ascending = False).drop_duplicates(
-        
+    emp1 =  employee.sort_values(by=['salary'],ascending = False).drop_duplicates(
+        subset='salary'
     )
     if 2> len(emp1):
         return pd.DataFrame({'SecondHighestSalary': [None]})
-    n = emp1.iloc[n-1]
+    n = emp1['salary'].iloc[n-1]
     
     return pd.DataFrame({'SecondHighestSalary': [n]})
     
